12fd157d90813c5dc6ed6d1f29c2b1b30dc55b47
ohara-h <k151656@kansai-u.ac.jp>
2023-10-31 21:28:07 +0900

    20231031

---
+++ sample/src/jp/co/diworks/action/Main.java
@@ -0,0 +1,16 @@
+package jp.co.diworks.action;
+
+public class Main {
+
+	public static void main(String[] args){
+System.out.println("Hello World");
+System.out.println(gokei(3,4));
+System.out.println(kakezan(5,5));
+	}
+    public static int gokei(int a, int b){
+    	return a+b;
+    }
+    public static int kakezan(int a, int b){
+    	return a*b;
+    }
+}
